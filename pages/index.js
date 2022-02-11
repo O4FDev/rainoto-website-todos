a17f6822bf764d1dab0f65751ade183a28322a16
O4FDev <luke.lucas@ou.ac.uk>
2022-02-11 19:06:03 +0000

    :sparkles:

--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ const Home = ({todos}) => {
             <div className="flex items-center justify-between">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
+                onSubmit={postData()}
               >
                 Submit
               </button>
